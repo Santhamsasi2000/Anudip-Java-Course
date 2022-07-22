032bdb211480334de87928eec68ee4e27e89d549
Santhamsasi2000 <109031565+Santhamsasi2000@users.noreply.github.com>
2022-07-22 10:05:18 +0530

    Add files via upload

---
+++ Lab Session/Lab Programs/18.07.2022 - Programs/ScannerLaptopDemo.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+
+
+public class ScannerLaptopDemo {
+  public static void main(String[] args) 
+  {
+			String[] MyArray = new String[7];
+			MyArray[0] = "				";
+			MyArray[1] = " SSSS  KKKKK   SSSSSS";
+			MyArray[2] = "  SSSS K   K     SSSS";
+			MyArray[3] = "  S    K K       S";
+			MyArray[4] = "    S  K           S";
+			MyArray[5] = "    S  K K         S ";
+			MyArray[6] = " SSSS  K   K	  SSSS";
+			
+			for (int i = 1; i < MyArray.length; i++)
+			{
+
+				System.out.println(MyArray[i]);
+			}
+
+			}
+		}
+	
